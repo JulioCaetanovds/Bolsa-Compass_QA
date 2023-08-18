e9be531e9c562dfa88c5cdd3e5df900f0908abae
JulioCaetanovds <128517358+JulioCaetanovds@users.noreply.github.com>
2023-08-18 16:02:38 -0300

    debug com gem pry

---
+++ RubyTDD/specs/calculadora_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'pry'
+require '../src/calculadora.rb'
+
+describe('Calculadora') do
+    calculadora = Calculadora.new
+
+    it('Deve somar dois inteiros positivos') do
+        expect(calculadora.soma(4, 5)).to eq 9
+    end
+
+    it('Deve somar um número qualquer com 0') do
+        expect(calculadora.soma(9, 0)).to eq 9
+    end
+
+    it('Deve somar numeros negativos') do
+        expect(calculadora.soma(-9, -10)).to eq -19
+    end
+
+    it('Deve subtrair dois inteiros positivos') do
+        binding.pry
+        expect(calculadora.subtrai(8, 2)).to eq 6
+    end
+end
